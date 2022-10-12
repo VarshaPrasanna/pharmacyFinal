4a5fdb73d08d4a795f36932c277b3e2b9ba27ad6
me <kj821801@student.nitw.ac.in>
2022-10-12 18:53:12 +0530

    js

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -26,7 +26,9 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlickCarouselModule
+    SlickCarouselModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     HomeComponent,
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup , FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  formSubmitted = false;
+  loginForm!: FormGroup; //this represents whole form
+  userName!: FormControl;
+  password!: FormControl;
   constructor() { }
 
   ngOnInit(): void {
+
+    this.userName = new FormControl('',[Validators.required]);
+    this.password = new FormControl('',[Validators.required]);
+
+    this.loginForm = new FormGroup({
+      'userName' : this.userName,
+      'password' : this.password
+    })
   }
 
 }
