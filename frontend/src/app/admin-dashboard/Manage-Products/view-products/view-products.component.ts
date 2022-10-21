90ee0657a99d46fd09fe1f4412c474cd7c96af52
Balwant Yadav <balwant7e@gmail.com>
2022-10-21 10:52:23 +0530

    Side nav in VIew Products

--- frontend/src/app/admin-dashboard/Manage-Products/view-products/view-products.component.ts
+++ frontend/src/app/admin-dashboard/Manage-Products/view-products/view-products.component.ts
@@ -3,6 +3,12 @@ import { ProductService } from '../product.service';
 import { Product } from '../models/product';
 //import { ProductMockData } from 'src/app/mock-data/products-mock-data';
 import { Router, ActivatedRoute } from '@angular/router';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
 @Component({
   selector: 'app-view-products',
   templateUrl: './view-products.component.html',
@@ -10,6 +16,14 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ViewProductsComponent implements OnInit {
 
+  title = 'admin-sidenav';
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+
+  onToggleSideNav(data: SideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+  }
   product!: any;
 
   constructor(
